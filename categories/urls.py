9f9c887914cc681ff91240455cf368142a5cb8f6
Mingyu Kil <90road@gmail.com>
2022-11-04 15:09:49 +0900

    REST Framework

--- categories/urls.py
+++ categories/urls.py
@@ -5,10 +5,23 @@ from django.urls import path
 urlpatterns = [
     path(
         "",
-        views.categories,
+        views.CategoryViewSet.as_view(
+            {
+                "get": "list",
+                "post": "create",
+            }
+        ),
     ),
     path(
         "<int:pk>",
-        views.category,
+        views.CategoryViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "partial_update",
+                "delete": "destroy",
+            }
+        ),
     ),
 ]
+
+"""as_view : get 이면 get 실행, post 이면 post 실행"""
